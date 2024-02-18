97b5bc7b04ca3351560066b119baf5fc8f49e290
fdiengott <freddy.diengott@proton.me>
2024-02-18 12:23:38 -0500

    add new keybindings and improve default ones

--- .config/nvim/lua/core/keybindings/keybindings.lua
+++ .config/nvim/lua/core/keybindings/keybindings.lua
@@ -1,4 +1,6 @@
 local keymap = vim.api.nvim_set_keymap
+
+vim.opt.cursorline = true
 local opts = { noremap = true, silent = true }
 
 local nmap = function(keys, func)
@@ -11,9 +13,6 @@ local imap = function(keys, func)
 	vim.api.nvim_set_keymap("i", keys, func, opts)
 end
 
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- switch between windows
 nmap("<C-h>", "<C-w>h")
@@ -25,8 +24,10 @@ nmap("<C-l>", "<C-w>l")
 nmap("<leader>h", ":nohlsearch<CR>")
 
 -- go to function/variable declarations
--- keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+
+nmap("<leader>/", "gcc")
 
 -- File Navigation
 -- includes dot files
@@ -38,8 +39,8 @@ nmap("(", "0")
 nmap(")", "$")
 
 -- Navigate buffers
-nmap("<leader>b", ":bnext<CR>")
-nmap("<leader>p", ":bprevious<CR>")
+nmap("<leader>]", ":bnext<CR>")
+nmap("<leader>[", ":bprevious<CR>")
 
 -- Insert
 imap("jk", "<ESC>")
@@ -49,3 +50,29 @@ vmap("<C-k>", "<ESC>")
 
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- shift highlighted lines up and down
+vmap("J", ":m '>+1<CR>gv=gv")
+vmap("K", ":m '<-2<CR>gv=gv")
+
+-- when joining lines, keep cursor in the same spot
+nmap("J", "mzJ`z")
+
+-- ctrl up and down keep the cursor in the center of the screen
+nmap("<C-d>", "<C-d>zz")
+nmap("<C-u>", "<C-u>zz")
+
+-- when matching text, keep center aligned
+nmap("n", "nzz")
+nmap("N", "Nzz")
+
+-- paste without putting the deleted text on the clipboard
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- leader y yanks to OS clipboard so vim can have internal clipboard
+nmap("<leader>y", "\"+y")
+vmap("<leader>y", "\"+y")
+nmap("<leader>Y", "\"+Y")
+
+-- starts a find and replace for the word under the cursor
+nmap("<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
