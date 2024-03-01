41d1a75d2368ef101fc8767c4f865e4cc6f84108
fdiengot <freddy.diengott@morganstanley.com>
2024-03-01 17:56:20 -0500

    update toggleterm keybindings

--- .config/nvim/lua/keybindings/toggleterm.lua
+++ .config/nvim/lua/keybindings/toggleterm.lua
@@ -10,10 +10,10 @@ function _G.set_terminal_keymaps()
 	local opts = { buffer = 0 }
 	vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
 	vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-	-- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-	-- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-	-- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-	-- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+	vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+	vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+	vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+	vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 	-- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
