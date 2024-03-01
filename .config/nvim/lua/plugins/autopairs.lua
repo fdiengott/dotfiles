182f6b377812fc18f76f12fbcd12045fe110f085
fdiengott <freddy.diengott@proton.me>
2024-03-01 02:06:03 -0500

    add autopairs plugin
    this plugin helps when creating html tags

---
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.set()
+    local npairs = require "nvim-autopairs"
+
+    npairs.setup {
+        check_ts = true,
+    }
+    npairs.add_rules(require "nvim-autopairs.rules.endwise-lua")
+end
+
+return M
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 require('plugins.nvim-tree')
 require('plugins.telescope')
+require('plugins.autopairs')
 
 -- add plugins here or in other files in this directory!
 return {}
