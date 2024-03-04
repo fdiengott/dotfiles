d48a0d1d5f24a05995cbd375dc142d0d3e3fc979
fdiengot <freddy.diengott@morganstanley.com>
2024-03-04 15:54:52 -0500

    update copilot keymap

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ require('plugins.nvim-tree')
 require('plugins.telescope')
 require('plugins.autopairs')
 require('plugins.oil')
+require('plugins.copilot')
 
 -- add plugins here or in other files in this directory!
 return {}
