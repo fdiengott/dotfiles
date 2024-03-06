b8c7d5f8ba14848ed9898aeb986f9470b9f18270
fdiengot <freddy.diengott@morganstanley.com>
2024-03-06 18:35:41 -0500

    update console snippets

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -5,8 +5,13 @@ local insert = luasnip.insert_node
 
 -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#loaders
 luasnip.add_snippets("javascript", {
-    snippet({ trig = "cc", name = "console.count" }, {
+    snippet({ trig = "count", name = "console.count" }, {
         text("console.count("),
+        insert(1),
+        text(");"),
+    }),
+    snippet({ trig = "log", name = "console.log" }, {
+        text("console.log("),
         insert(1),
         text(");"),
     }),
