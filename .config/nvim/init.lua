3f7f1983b07d6783a7ec7c3dd8dbee139ebe64f6
fdiengott <freddy.diengott@proton.me>
2024-03-07 01:01:08 -0500

    change colortheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -172,14 +172,18 @@ require('lazy').setup({
 
   -- color theme
   {
-    --   'navarasu/onedark.nvim',
-    --   "folke/tokyonight.nvim",
-    'rebelot/kanagawa.nvim',
+    -- 'navarasu/onedark.nvim',
+    -- 'folke/tokyonight.nvim',
+    -- 'rebelot/kanagawa.nvim',
+    -- 'Yazeed1s/minimal.nvim',
+    'oxfist/night-owl.nvim',
     lazy = false,
     priority = 1000,
     config = function()
       -- vim.cmd([[colorscheme tokyonight-night]])
-      vim.cmd([[colorscheme kanagawa-wave]])
+      -- vim.cmd([[colorscheme kanagawa-wave]])
+      -- vim.cmd([[colorscheme minimal]])
+      vim.cmd([[colorscheme night-owl]])
     end,
   },
   {
--- .config/nvim/lua/settings/colors.lua
+++ .config/nvim/lua/settings/colors.lua
@@ -1,2 +1,7 @@
 -- COLORS / guibg for background, guifg for foreground
-vim.cmd("highlight Normal guibg=#000000")
+-- vim.cmd("highlight Normal guibg=#000000")
+-- vim.cmd("highlight @tag guifg=#7fccf0")
+-- vim.cmd("highlight @variable guifg=#d49adb")
+
+vim.cmd("highlight Normal guibg=#000408")
+vim.cmd("highlight CursorLine guibg=#222428")
