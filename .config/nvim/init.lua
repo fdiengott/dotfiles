1c134d429af676e65c2f700d163b43707fe0f5e1
fdiengot <freddy.diengott@morganstanley.com>
2024-03-12 18:05:33 -0400

    add the diffview plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,6 +46,8 @@ require('lazy').setup({
   'tpope/vim-ragtag',
   'stevearc/oil.nvim',
 
+  'sindrets/diffview.nvim',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
