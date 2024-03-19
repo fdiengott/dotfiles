7fd1e2742434d1cbecc5f98ce03462ba0810d4a2
fdiengot <freddy.diengott@morganstanley.com>
2024-03-19 10:41:34 -0400

    add a new snippet and setup keybindings

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -2,6 +2,9 @@ local luasnip = require 'luasnip'
 local snippet = luasnip.snippet
 local text = luasnip.text_node
 local insert = luasnip.insert_node
+local extras = require("luasnip.extras")
+local rep = extras.rep -- this will repeat whatever is in the corresponding insert
+local fmt = require("luasnip.extras.fmt").fmt
 
 -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#loaders
 luasnip.add_snippets("javascript", {
@@ -15,4 +18,10 @@ luasnip.add_snippets("javascript", {
         insert(1),
         text(");"),
     }),
+    snippet("caf", fmt("const {} = ({}) => {{\n\t{}\n}};", { insert(1), insert(2), insert(3) })),
 })
+
+-- example of repeat
+-- luasnip.add_snippets("all", {
+--     snippet("test", fmt("local {} = require '{}'", { insert(1), rep(1) })),
+-- })
