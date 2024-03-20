a7b21fe316387de19e143ce1bdd0b05efaa03266
fdiengot <freddy.diengott@morganstanley.com>
2024-03-20 13:08:42 -0400

    set keybind that wont conflict for copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,6 @@
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false
 })
-vim.g.copilot_no_tab_map = true
 
 vim.keymap.set('n', '<leader>gc', ':CopilotChatToggle<CR>', { desc = 'Toggle [c]opilot chat' });
