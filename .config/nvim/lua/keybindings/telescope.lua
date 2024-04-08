e0e9ed873a2dd07291b37317e75eed9e6b4ea0ad
fdiengott <freddy.diengott@proton.me>
2024-04-08 00:21:03 -0400

    update telescope keybindings

--- .config/nvim/lua/keybindings/telescope.lua
+++ .config/nvim/lua/keybindings/telescope.lua
@@ -1,15 +1,15 @@
 local builtin = require('telescope.builtin')
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>/', builtin.oldfiles, { desc = '[/] Find recently opened files' })
+vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch [r]ecently opened files' })
 vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>f', function()
+vim.keymap.set('n', '<leader>sl', function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
 		winblend = 10,
 		previewer = false,
 	})
-end, { desc = '[/] Fuzzily search in current buffer' })
+end, { desc = '[S]earch in [l]ocal buffer' })
 
 local function telescope_live_grep_open_files()
 	builtin.live_grep {
@@ -17,13 +17,13 @@ local function telescope_live_grep_open_files()
 		prompt_title = 'Live Grep in Open Files',
 	}
 end
-vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
+vim.keymap.set('n', '<leader>so', telescope_live_grep_open_files, { desc = '[S]earch in [o]pen Files' })
 -- vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>ps', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>ps', builtin.git_files, { desc = '[p]roject [s]earch Git files' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [f]iles' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [h]elp' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [w]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [g]rep' })
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
 -- vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
