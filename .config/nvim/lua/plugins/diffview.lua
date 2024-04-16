349bd920da23aa1ce041e152fd55e1ee193ddfc6
fdiengot <freddy.diengott@morganstanley.com>
2024-04-16 16:59:25 -0400

    update comment and clean up config

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,5 +3,9 @@ return {
     config = function()
         vim.keymap.set('n', '<leader>hd', ":DiffviewFileHistory %<CR>", { desc = 'git diff file' })
         vim.keymap.set('n', '<leader>hD', ":DiffviewFileHistory<CR>", { desc = 'git diff branch' })
+
+        -- for the merge tool, when there is a merge conflict just run the command :Diffviewopen
+        -- use [x or ]x to merge between conflicts
+        -- use <leader>c (with which key) to see options for merging in OURS or THEIRS
     end
 }
