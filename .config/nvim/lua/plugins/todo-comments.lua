2c02cf2a70376a64088390fd2d844bf9136a96c0
fdiengot <freddy.diengott@morganstanley.com>
2024-04-25 12:06:29 -0400

    add ASK todo comment

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,14 +1,13 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-        keywords = {
-            ASK = { icon = "?", color = "hint", alt = { "QUESTION" } }
-        },
-        merge_keywords = true,
-    },
     config = function()
-        require('todo-comments').setup({})
+        require('todo-comments').setup({
+            keywords = {
+                ASK = { icon = "?", color = "warning", alt = { "QUESTION", "Q" } },
+            },
+            merge_keywords = true,
+        })
 
         vim.keymap.set("n", "]t", function()
             require("todo-comments").jump_next()
