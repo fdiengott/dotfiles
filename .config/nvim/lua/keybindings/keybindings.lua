854083c9f7f1e37172083ea5d53980ff9fc2190d
fdiengot <freddy.diengott@morganstanley.com>
2024-05-24 12:44:05 -0400

    remove custom relative line number keybinding
    removed because it is redundant due to unimpaired

--- .config/nvim/lua/keybindings/keybindings.lua
+++ .config/nvim/lua/keybindings/keybindings.lua
@@ -20,9 +20,6 @@ nmap("<leader>ss", ":source %<CR>", '[ss]ource')
 -- Open root init.lua file
 nmap("<leader>vi", ":tabe $MYVIMRC<CR>", 'Open [vi]m config')
 
--- toggle relative line numbers
-nmap("<leader>rl", ":set relativenumber!<CR>", 'Toggle [r]elative line numbers')
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
