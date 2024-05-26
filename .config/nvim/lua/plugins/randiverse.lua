976d54c0d12810438ee11974accfdd90b97c56ce
fdiengott <freddy.diengott@proton.me>
2024-05-26 15:42:37 -0400

    add randiverse for random value generation

---
+++ .config/nvim/lua/plugins/randiverse.lua
@@ -0,0 +1,9 @@
+return {
+    "ty-labs/randiverse.nvim",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("randiverse").setup({
+            -- Custom configurations here, or leave empty to use defaults
+        })
+    end
+}
