e4abbbdbc3c93e45ec569f4f6c0489d26abc12a1
fdiengott <freddy.diengott@proton.me>
2024-05-26 16:14:48 -0400

    add codeium

---
+++ .config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,21 @@
+if is_linux() then
+    return {
+        'Exafunction/codeium.vim',
+        config = function()
+            vim.keymap.set('i', '<c-a>', function()
+                return vim.fn['codeium#Accept']()
+            end, { expr = true })
+            -- vim.keymap.set('i', '<c-,>', function()
+            --     return vim.fn['codeium#CycleCompletions'](1)
+            -- end, { expr = true })
+            -- vim.keymap.set('i', '<c-.>', function()
+            --     return vim.fn['codeium#CycleCompletions'](-1)
+            -- end, { expr = true })
+            -- vim.keymap.set('i', '<c-x>', function()
+            --     return vim.fn['codeium#Clear']()
+            -- end, { expr = true })
+        end
+    }
+else
+    return {}
+end
