ba7d1e406279856863e364781e735348a5b6650d
fdiengott <freddy.diengott@proton.me>
2024-05-29 11:07:09 -0400

    update codeium and copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,15 @@
-return {
-    'github/copilot.vim',
-    config = function()
-        vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")', {
-            expr = true,
-            replace_keycodes = false
-        })
+if is_mac() then
+    return {
+        'github/copilot.vim',
+        config = function()
+            vim.keymap.set('i', '<C-a>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
+            })
 
-        vim.g.copilot_no_tab_map = true
-    end
-}
+            vim.g.copilot_no_tab_map = true
+        end
+    }
+else
+    return {}
+end
