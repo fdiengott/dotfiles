bd8ee9315158d977b947ae884449a4c66155e4e2
fdiengott <freddy.diengott@proton.me>
2024-06-13 10:11:22 -0400

    improve oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,13 @@ return {
             keymaps = {
                 ["<C-h>"] = false,
                 ["<C-l>"] = false,
+            },
+            delete_to_trash = true,
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name, _)
+                    return name == '..' or name == '.git'
+                end
             }
         })
 
