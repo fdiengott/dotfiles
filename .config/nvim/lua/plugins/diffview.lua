58bb3d05be9b021cb84ba962f173b3b38eee3c1f
fdiengott <freddy.diengott@proton.me>
2024-08-30 11:45:42 -0400

    update diffview close keybinding

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -3,8 +3,8 @@ return {
     config = function()
         vim.keymap.set('n', '<leader>hd', ":DiffviewFileHistory %<CR>", { desc = 'git diff file' })
         vim.keymap.set('n', '<leader>hD', ":DiffviewFileHistory<CR>", { desc = 'git diff branch' })
+        vim.keymap.set('n', '<leader>hc', ":DiffviewClose<CR>", { desc = '[d]iffview [c]lose' })
         vim.keymap.set('n', '<leader>mc', ":DiffviewOpen<CR>", { desc = 'handle [m]erge [c]onflicts' })
-        vim.keymap.set('n', '<leader>dc', ":DiffviewClose<CR>", { desc = '[d]iffview [c]lose' })
         -- use [x or ]x to merge between conflicts
         -- use <leader>c (with which key) to see options for merging in OURS or THEIRS
     end
